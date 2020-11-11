8a099687fd33460f2425a85ca714fc01c277c7ed
bb35319011 <bb35319011@ms.nagasaki-u.ac.jp>
2020-11-11 13:32:32 +0900

    力ずく法・BM法実装

--- k03/k03.c
+++ k03/k03.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define DEBUG
 
@@ -10,12 +11,83 @@ char StrKey[] = "wind";
 char* ForceSearch(char text[], char key[])
 {
     //  ここを実装する
+    char* ch ;
+    int start;
+    int pos;
 
+    for ( start=0; text[start] != NULL; start++)
+    {
+        ch = &text[start];//chに検索開始位置を保持
+        for (pos = 0; key[pos] != NULL; pos++)
+        {
+            if (text[start+pos] != key[pos])//一文字ずつ比較
+            {
+                ch = NULL;
+                break;
+            }
+        }
+        if(ch != NULL) //最後まで一致したらreturn
+        {
+            ch[pos]="\0";
+            return ch;
+        }
+    }
+
+    return NULL;
 }
 
 char* BMSearch(char text[], char key[])
 {
     //  ここを実装する
+    char* ch;
+    int start,pos;
+    int keyLength=0;
+    int *table[256];
+    
+    //文字数しゅとく
+    while (key[keyLength]!=NULL)
+    {
+        keyLength++;
+    }
+
+    //ずらし両テーブル作成
+    for(int i=0; i<256; i++)
+    {
+        table[i]=keyLength;
+    }
+    for (int i = 0; key[i]!=NULL; i++)
+    {
+        table[key[i]]=(keyLength-1)-i;
+    }
+
+    //比較
+    start=keyLength-1;  //検索開始位置の初期化
+    while(text[start] != NULL)
+    {
+        ch =&text[start-(keyLength-1)];  //検索する位置の先頭アドレスを保持
+        for(pos=0; key[pos]!=0; pos++)
+        {
+            if(text[start-pos] != key[(keyLength-1)-pos])//keyの後ろから一文字ずつ比較
+            {
+                //一致しなかった場所から開始位置をずらす
+                if(table[text[start-pos]] > pos)
+                {
+                    start += (int)table[text[start-pos]] - pos;
+                }else{  //開始位置がループしないための処理
+                    start++;
+                }
+                ch=NULL;
+                break;
+            }
+        }
+        if(ch !=NULL)//最後まで一致したらreturn
+        {
+            ch[pos]="\0";
+            return ch;
+        }
+    }
+
+    return NULL;
 
 }
 
